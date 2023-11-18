7c2d91dce056ecdebd48ff83bd43ad3cfd5d0937
shiroe66 <89994320+shiroe66@users.noreply.github.com>
2023-11-18 15:26:10 +0500

    fix: build chuck size

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,13 @@
-import { defineConfig } from 'vite';
+import { defineConfig, splitVendorChunkPlugin } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/minesweeper_test/',
+  build: {
+    chunkSizeWarningLimit: 1600,
+  },
+  base: '/minesweeper',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
